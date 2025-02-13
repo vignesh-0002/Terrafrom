8a12b364ee4ba34652407bee3414894fdadeb2d8
vignesh-0002 <65174215+vignesh-0002@users.noreply.github.com>
2025-02-13 23:09:54 +0530

    Update provider.tf

--- Github-actions/provider.tf
+++ Github-actions/provider.tf
@@ -19,9 +19,16 @@ terraform {
 
 # Configure the AWS Provider
 
-provider "aws" {
-  profile = "AWS-Secrets"  # Ensure this profile exists in ~/.aws/credentials
-  region  = "us-east-1"    # Specify the AWS region
-}
+#provider "aws" {
+#  profile = "AWS-Secrets"  # Ensure this profile exists in ~/.aws/credentials
+#  region  = "us-east-1"    # Specify the AWS region
+#}
 
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
 
